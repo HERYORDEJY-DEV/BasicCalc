1e7ee5c354617e90ec298dff84c69b759288b87c
HERYORDEJY <heryordejy.dev@gmail.com>
2023-05-27 19:01:51 +0100

    Implemented basic of calculator functionality

---
+++ app/src/main/java/dev/heryordejy/basiccalc/MainActivity.kt
@@ -0,0 +1,83 @@
+package dev.heryordejy.basiccalc
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnAddition = findViewById<Button>(R.id.btnAddition)
+        val btnSubtraction = findViewById<Button>(R.id.btnSubtraction)
+        val btnMultiplication = findViewById<Button>(R.id.btnMultiplcation)
+        val btnDivision = findViewById<Button>(R.id.btnDivision)
+        val firstOperand = findViewById<EditText>(R.id.firstOperand)
+        val secondOperand = findViewById<EditText>(R.id.secondOperand)
+
+
+        btnAddition.setOnClickListener {
+            val result = findViewById<TextView>(R.id.result)
+
+            if (firstOperand.text.isEmpty() || secondOperand.text.isEmpty()) {
+                btnAddition.isEnabled = false
+                Log.d("TEst ing", "--------")
+            } else {
+                val firstOperandValue = firstOperand.text.toString().toInt()
+                val secondOperandValue = secondOperand.text.toString().toInt()
+                val calc = firstOperandValue + secondOperandValue
+                result.text = "Result is $calc"
+            }
+
+        }
+
+        btnSubtraction.setOnClickListener {
+            val result = findViewById<TextView>(R.id.result)
+
+            if (firstOperand.text.isEmpty() || secondOperand.text.isEmpty()) {
+                btnSubtraction.isEnabled = false
+            } else {
+                val firstOperandValue = firstOperand.text.toString().toInt()
+                val secondOperandValue = secondOperand.text.toString().toInt()
+                val calc = firstOperandValue - secondOperandValue
+                result.text = "Result is $calc"
+            }
+
+        }
+
+        btnMultiplication.setOnClickListener {
+            val result = findViewById<TextView>(R.id.result)
+
+            if (firstOperand.text.isEmpty() || secondOperand.text.isEmpty()) {
+                btnMultiplication.isEnabled = false
+            } else {
+                val firstOperandValue = firstOperand.text.toString().toInt()
+                val secondOperandValue = secondOperand.text.toString().toInt()
+                val calc = firstOperandValue * secondOperandValue
+                result.text = "Result is $calc"
+            }
+
+        }
+
+        btnDivision.setOnClickListener {
+            val result = findViewById<TextView>(R.id.result)
+
+            if (firstOperand.text.isEmpty() || secondOperand.text.isEmpty()) {
+                btnDivision.isEnabled = false
+            } else {
+                val firstOperandValue = firstOperand.text.toString().toInt()
+                val secondOperandValue = secondOperand.text.toString().toInt()
+                val calc = firstOperandValue / secondOperandValue
+                result.text = "Result is $calc"
+            }
+
+        }
+    }
+
+
+}
